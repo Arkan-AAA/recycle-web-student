69daad9b5915c1fdb88dee570d96fed88cc1066d
AAlikhan-prog <DmsPlay228@gmail.com>
2026-02-04 08:30:13 +0500

    Create directory for work

---
+++ frontend/src/components/articles/CommentSection.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "CommentSection"
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/pages/NewsPage.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "NewsPage"
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
